f13d1c99c77be62151a52e4c2ca4eacdd28dfd32
AkankshyaSantosh <67215928+AkankshyaSantosh@users.noreply.github.com>
2020-07-18 19:14:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+document.getElementById("time").innerHTML = Date();
+
+function copy() {
+    alert("LOL ! You can't Copy")
+}
+
+function summer() {
+    document.body.style.backgroundImage = "url('https://png.pngtree.com/thumb_back/fh260/background/20190221/ourmid/pngtree-summer-beach-coconut-tree-hot-air-balloon-image_27169.jpg')"
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("Hey ! It's Summer!!")
+}
+
+function winter() {
+    document.body.style.backgroundImage = "url('https://ak0.picdn.net/shutterstock/videos/923260/thumb/1.jpg')"
+    document.body.style.backgroundSize = "100%";
+    alert("Hey ! It's Winter!!")
+}
+
+function autumn() {
+    document.body.style.backgroundImage = "url('https://ak3.picdn.net/shutterstock/videos/12828998/thumb/1.jpg?i10c=img.resize(height:160')"
+    document.body.style.backgroundSize = "100%";
+    alert("Hey ! It's Autumn!!")
+}
+
+function spring() {
+    document.body.style.backgroundImage = "url('https://th.bing.com/th/id/OIP.7b7PEUlE_IvLOIYfoAIKxwHaDt?pid=Api&rs=1')"
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("Hey ! It's Spring!!")
+}
